621eda03a2524beef8fc6cda5e86235dcc77641c
Oleh Novatskyi <oleh.novatskyi@spryker.com>
2021-10-06 19:02:51 +0300

    APPS-1749: Fixed Travis CI erros #2

--- src/Spryker/Zed/ApplicationCatalogGui/ApplicationCatalogGuiConfig.php
+++ src/Spryker/Zed/ApplicationCatalogGui/ApplicationCatalogGuiConfig.php
@@ -54,7 +54,9 @@ class ApplicationCatalogGuiConfig extends AbstractBundleConfig
      */
     public function getTenantUuid(): string
     {
-        return getenv('TENANT_UUID') ?: (getenv('SPRYKER_BE_HOST') ?: '');
+        $beHostInsteadOfTenantUuid = getenv('SPRYKER_BE_HOST') !== false ? getenv('SPRYKER_BE_HOST') : 'TENANT_UUID_UNDEFINED';
+
+        return getenv('TENANT_UUID') !== false ? getenv('TENANT_UUID') : $beHostInsteadOfTenantUuid;
     }
 
     /**
